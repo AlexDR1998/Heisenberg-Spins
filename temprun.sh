7d159184c4e743fd15751562e43c41db22e03191
Alex <alexander.richardson@gmail.com>
2019-06-11 21:05:33 +0100

    push bash script

---
+++ temprun.sh
@@ -0,0 +1,27 @@
+#compile c++ script first
+#g++ -std=c++11 -g -O3 -mcmodel=medium source.cpp -o outp.o
+g++ -std=c++11 -g -O3 CH_MC_heatbath.cpp -o outp.o
+#g++ -std=c++11 -g -O3 IL_MC_heatbath_split.cpp -o outp.o
+#empties the output files
+>energy.txt
+>energy2.txt
+>spin_total.txt
+>spin2_total.txt
+>spins.txt
+>spins2.txt
+>spin_corr.txt
+
+#temperature step size
+dT=0.2
+
+#initial high temperature run
+./outp.o none 100 1
+
+#cooling system
+for j in $(seq 500 -1 1); do
+kT=$(echo "scale=4; $j*$dT"|bc)
+./outp.o spins_after.txt $kT 1
+done
+
+
+
