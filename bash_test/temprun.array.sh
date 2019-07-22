bb274b2f2c1b44f6e7388850c6cc4686d0929dcf
Alex <alexander.richardson@gmail.com>
2019-07-22 12:10:54 +0100

    Trying out eddie job arrays

---
+++ bash_test/temprun.array.sh
@@ -0,0 +1,7 @@
+#! /bin/sh
+#$ -N CH_cooling
+#$ -cwd
+#$ -l h_rt=100:00:00
+#$ -l h_vmem=16G
+
+temprun.sh
---
+++ bash_test/temprun.sh
@@ -0,0 +1,48 @@
+#! /bin/sh
+#$ -N CH_cooling
+#$ -cwd
+#$ -l h_rt=100:00:00
+#$ -l h_vmem=16G
+#compile c++ script first
+#g++ -std=c++11 -g -O3 -mcmodel=medium source.cpp -o outp.o
+g++ -std=c++11 -g -O2 -mcmodel=medium ../CH_MC_heatbath.cpp -o outp.o
+#g++ -std=c++11 -g -O3 IL_MC_heatbath_split.cpp -o outp.o
+#empties the output files
+>energy.txt
+>energy2.txt
+>spin_total.txt
+>spin_total_even.txt
+>spin_total_even.txt
+>spin2_total.txt
+>spin2_total_even.txt
+>spin2_total_odd.txt
+>spins.txt
+>spins2.txt
+>spin_corr.txt
+
+#Set parameters
+dT=0.05
+u0=$SGE_TASK_ID
+
+#initial high temperature run
+./outp.o none 5 1 $u0
+
+#cooling system
+for j in $(seq 99 -1 1); do
+kT=$(echo "scale=4; $j*$dT"|bc)
+./outp.o spins_after.txt $kT 1 $u0
+done
+
+
+#Store results
+mkdir result$u0
+cp energy.txt result$u0
+cp energy2.txt result$u0
+cp spin_total.txt result$u0
+#cp spin_total_even.txt result$u0
+#cp spin_total_odd.txt result$u0
+cp spin2_total.txt result$u0
+#cp spin2_total_even.txt result$u0
+#cp spin2_total_odd.txt result$u0
+cp spins.txt result$u0
+cp spins2.txt result$u0
