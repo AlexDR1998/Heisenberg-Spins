7da2b3cbd87ef3a9ab5a4a524f31d3593d76a4b9
RICHARDSON Alex <s1605376@login04.ecdf.ed.ac.uk>
2019-09-30 13:03:40 +0100

    Adding bash scripts to git

--- temprun.sh
+++ temprun.sh
@@ -1,32 +1,56 @@
 #! /bin/sh
-#$ -N CH_cooling
+#$ -N HL_cooling
 #$ -cwd
-#$ -l h_rt=04:00:00
-#$ -l h_vmem=8G
+#$ -l h_rt=100:00:00
+#$ -l h_vmem=16G
 #compile c++ script first
 #g++ -std=c++11 -g -O3 -mcmodel=medium source.cpp -o outp.o
-g++ -std=c++11 -g -O3 -mcmodel=medium CH_MC_heatbath.cpp -o outp.o
 #g++ -std=c++11 -g -O3 IL_MC_heatbath_split.cpp -o outp.o
 #empties the output files
+#SGE_TASK_ID=1
+mkdir result$SGE_TASK_ID
+#cp outp result$1
+cp system.txt result$SGE_TASK_ID
+cd result$SGE_TASK_ID
+
+g++ -std=c++11 -g -O2 -mcmodel=medium ../../HL_MC_heatbath.cpp -o outp
+
 >energy.txt
 >energy2.txt
 >spin_total.txt
+>spin_total_even.txt
+>spin_total_even.txt
 >spin2_total.txt
+>spin2_total_even.txt
+>spin2_total_odd.txt
 >spins.txt
 >spins2.txt
 >spin_corr.txt
+>spins_after.txt
 
-#temperature step size
-dT=0.2
+#Set parameters
+dT=0.05
+#u0=$SGE_TASK_ID
 
 #initial high temperature run
-./outp.o none 100 1
+./outp none 5 1 $SGE_TASK_ID
 
 #cooling system
-for j in $(seq 500 -1 1); do
+for j in $(seq 99 -1 1); do
 kT=$(echo "scale=4; $j*$dT"|bc)
-./outp.o spins_after.txt $kT 1
+./outp spins_after.txt $kT 1 $SGE_TASK_ID
 done
 
 
+#Store results
 
+#cp energy.txt result$SGE_TASK_ID
+#cp energy2.txt result$SGE_TASK_ID
+#cp spin_total.txt result$SGE_TASK_ID
+#cp spin_total_even.txt result$SGE_TASK_ID
+#cp spin_total_odd.txt result$SGE_TASK_ID
+#cp spin2_total.txt result$SGE_TASK_ID
+#cp spin2_total_even.txt result$SGE_TASK_ID
+#cp spin2_total_odd.txt result$SGE_TASK_ID
+#cp spins.txt result$SGE_TASK_ID
+#cp spins2.txt result$SGE_TASK_ID
